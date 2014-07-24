c452dcebf83c7528a73e498626e5d543c3331c06
pkosmowski <pkosmowski@PLDL0097.rule.lan>
2014-07-24 14:38:52 +0200

    Add extra path for Rest resources

---
+++ TestingJaxRs/src/main/java/pl/pkosmowski/testing/jaxrs/web/rest/RestConfiguration.java
@@ -0,0 +1,13 @@
+package pl.pkosmowski.testing.jaxrs.web.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author pkosmowski
+ */
+@ApplicationPath("rest")
+public class RestConfiguration extends Application{
+    
+}
